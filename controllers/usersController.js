ebf010a7dfef56ea58cfd6c034b05d97c152379a
Carlos <110745188+carlDguez19@users.noreply.github.com>
2026-07-21 15:59:33 -0600

    removed a middleware func in the controller that was defined twice. kept the one with validation

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -29,11 +29,11 @@ exports.usersCreateGet = (req, res) => {
   });
 };
 
-exports.usersCreatePost = (req, res) => {
-  const { firstName, lastName } = req.body;
-  usersStorage.addUser({ firstName, lastName });
-  res.redirect("/");
-};
+// exports.usersCreatePost = (req, res) => {
+//   const { firstName, lastName } = req.body;
+//   usersStorage.addUser({ firstName, lastName });
+//   res.redirect("/");
+// };
 
 // We can pass an entire array of middleware validations to our controller.
 exports.usersCreatePost = [
